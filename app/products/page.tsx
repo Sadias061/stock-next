f97ee3f13297f671218b8ecb5cb88dcbe06a7793
AGNAWALE Esdras <agnawaleayantayoesdras@gmail.com>
2025-10-24 16:15:53 +0100

    Modification

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,28 +1,70 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "../components/Wrapper";
 import { useUser } from "@clerk/nextjs";
 import { Product } from "@/type";
+import { readProducts } from "../actions";
+import EmptyState from "../components/EmptyState";
 
-const page = () => {
-  // const { user } = useUser();
-  // const email = user?.primaryEmailAddress?.emailAddress as string;
-  // const [products, setProducts] = useState<Product[]>([]);
+const Page = () => {
+  const { user } = useUser();
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+  const [products, setProducts] = useState<Product[]>([]);
 
-  // const fetchProducts = async () => {
-  //   const response = await fetch(`/api/products?email=${email}`);
-  //   const data = await response.json();
-  //   setProducts(data);
-  // };
+  const fetchProducts = async () => {
+    try {
+      if (email) {
+        const products = await readProducts(email);
+        if (products) {
+          // Transformation des données pour garantir que categoryName est une chaîne
+          const formattedProducts = products.map((product) => ({
+            ...product,
+            categoryName: product.categoryName || "Inconnu", // Valeur par défaut
+          }));
+          setProducts(formattedProducts);
+        }
 
-  // fetchProducts();
+      }
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+
+  useEffect(() => {
+    if (email) {
+      fetchProducts();
+    }
+  }, [email]);
 
 
   return (
     <Wrapper>
-      <h1>Page produit</h1>
+      <div className="overflow-x-auto">
+        {products.length === 0 ? (
+         <p className="text-center mt-10">Produits non trouvé.</p>
+        ) : (
+          <table className="min-w-full bg-white border border-gray-200">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">Name</th>
+                <th className="py-2 px-4 border-b">Price</th>
+                <th className="py-2 px-4 border-b">Category</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product.id}>
+                  <td className="py-2 px-4 border-b">{product.name}</td>
+                  <td className="py-2 px-4 border-b">{product.price} €</td>
+                  <td className="py-2 px-4 border-b">{product.categoryName}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </Wrapper>
   );
 };
 
-export default page;
+export default Page;
